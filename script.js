80663d15278ad7c3fe613c07295b4164910b212f
Harrison K. <53415491+kungharrison@users.noreply.github.com>
2023-11-24 00:19:14 -0800

    updated no js invalid messages

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    // Perform your form validation here
-    var isValid = true; // Replace with your actual validation logic
+let form = document.getElementById('contact-form');
+let inputs = form.querySelectorAll('input, textarea');
+let formErrors = [];
 
-    var output = document.getElementById('contact-form-output');
-    if (isValid) {
-
-        output.innerHTML = 'Form submitted successfully!';
-        output.style.color = 'green';
+inputs.forEach(input => {
+  input.addEventListener('input', () => {
+    if (!input.checkValidity()) {
+      input.setCustomValidity('Invalid input');
+      formErrors.push({field: input.name, error: input.validationMessage});
     } else {
-        event.preventDefault(); // Prevent the form from submitting normally
-        output.innerHTML = 'There was an error submitting the form.';
-        output.style.color = 'red';
+      input.setCustomValidity('');
     }
+  });
+
+  input.addEventListener('keypress', (event) => {
+    let pattern = new RegExp(input.pattern);
+    if (!pattern.test(event.key)) {
+      event.preventDefault();
+      input.classList.add('flash');
+      setTimeout(() => input.classList.remove('flash'), 1000);
+      document.getElementById('contact-form-error-output').textContent = 'Illegal character';
+      setTimeout(() => document.getElementById('contact-form-error-output').textContent = '', 3000);
+    }
+  });
+});
+
+let comments = document.getElementById('message');
+comments.addEventListener('input', () => {
+  let remaining = comments.maxLength - comments.value.length;
+  document.getElementById('contact-form-info-output').textContent = `${remaining} characters remaining`;
+  if (remaining < 10) {
+    document.getElementById('contact-form-info-output').classList.add('warn');
+  } else {
+    document.getElementById('contact-form-info-output').classList.remove('warn');
+  }
+});
+
+form.addEventListener('submit', (event) => {
+  let formErrorsField = document.createElement('input');
+  formErrorsField.type = 'hidden';
+  formErrorsField.name = 'form-errors';
+  formErrorsField.value = JSON.stringify(formErrors);
+  form.appendChild(formErrorsField);
 });
